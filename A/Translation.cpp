e2c5881a7012eba8ba33b50ec80beeda4cd0b0c5
Hendawy <48328568+abbdelrhman@users.noreply.github.com>
2021-07-26 14:27:08 +0200

    Add files via upload

---
+++ A/Translation.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std ;
+int main() {
+    string  s1,s2 ;
+    cin>>s1>>s2;
+    reverse(s2.begin(),s2.end());
+    if (s1.compare(s2)==0){
+        cout <<"YES";
+    }else{
+        cout<<"NO";
+    }
+     return 0;
+}
